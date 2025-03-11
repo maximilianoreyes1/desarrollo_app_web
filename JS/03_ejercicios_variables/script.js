57df4cbc4498f2a3ef76485c632e683d2eaa3643
maximilianoreyes1 <maximilianoreyes@liceovvh.cl>
2025-03-11 10:51:39 -0300

    ok

--- JS/03_ejercicios_variables/script.js
+++ JS/03_ejercicios_variables/script.js
@@ -1,17 +1,18 @@
 //creacion de variables
-const rut = 22693875
-let numero = 0; //numero INT
+let numero = 6; //numero INT
 let decimal = 2.5 //numero float
 let texto = "Hola";//string
 let booleano = true;//booleano
+let indefinido = undefined;
+let nulo = null;
 
 let array = [1, "Hola", 25];//array, lista
 let matriz = [[1,2], [3,4]]
-let objeto = {nombre: "Maxi", Edad:16};
+let objeto = {nombre: "Maxi", edad:16};
 
-function sumar(a, b){
-    return a + b
-};
+function int(){
+    alert(numero)
+}
 
 function float(){
     alert(decimal);
@@ -21,6 +22,26 @@ function string(){
     alert(texto)
 }
 
-function boolean{
+function boolean(){
     alert(booleano)
+}
+
+function m_array(){
+    alert(array)
+}
+
+function m_matriz(){
+    alert(matriz)
+}
+
+function m_object(){
+    alert("Nombre: " + objeto.nombre + "\nEdad: " + objeto.edad)
+}
+
+function ind(){
+    alert(indefinido)
+}
+
+function m_nulo(){
+    alert(nulo)
 }
