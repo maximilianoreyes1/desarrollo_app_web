1761252c5c8d8fe069d0f0349e935a4d7e8712a4
maximilianoreyes1 <maximilianoreyes@liceovvh.cl>
2025-03-11 11:09:57 -0300

    ok

---
+++ JS/04_Ejercicios/script.js
@@ -0,0 +1,38 @@
+console.log("Vinculado correctamente...")
+// Ejercicio 1: Declaración de variables
+// Instrucciones: Declara dos variables numéricas llamadas 'ahora' y 'fecha_nac'.
+// Luego, calcula y muestra en un mensaje la edad actual basada en el año de nacimiento.
+
+let ahora = 2025;
+let fecha_nac = 2008;
+
+function declararVariables() {
+    // Tu código aquí
+    resultado = ahora - fecha_nac;
+    alert("Edad: " + resultado);
+    }
+// Ejercicio 2: Asignación de valores
+// Instrucciones: Completa la función para asignar el valor de 'var1' a 'var2' y luego muestra el valor de 'var2'.
+function asignarValores() {
+// Tu código aquí
+    
+}
+    
+// Ejercicio 3: Operaciones matemáticas
+// Instrucciones: Completa la función para realizar las siguientes operaciones:
+// Suma 'num1','num2' y 'num3', resta 'num1' de 'num3', multiplica 'num2' por 'num3', y divide 'num1' entre '120'.
+// Muestra los resultados en un mensaje.
+function realizarOperaciones() {
+
+alert( );
+
+// Tu código aquí
+}
+
+// Ejercicio 4: Manipulación de cadenas
+// Instrucciones: Completa la función para construir un mensaje utilizando las variables 'direccion' y 'n_casa'.
+// Luego, muestra la longitud del mensaje y el antepenultimo en el mensaje. (.length)
+function manipularCadenas() {
+
+// Tu código aquí
+}
