f68baee816b43e4d44c12ebbe783e71dc6d33cea
maximilianoreyes1 <maximilianoreyes@liceovvh.cl>
2025-03-11 21:54:46 -0300

    ok

--- JS/04_Ejercicios/script.js
+++ JS/04_Ejercicios/script.js
@@ -3,11 +3,10 @@ console.log("Vinculado correctamente...")
 // Instrucciones: Declara dos variables numéricas llamadas 'ahora' y 'fecha_nac'.
 // Luego, calcula y muestra en un mensaje la edad actual basada en el año de nacimiento.
 
-let ahora = 2025;
-let fecha_nac = 2008;
-
 function declararVariables() {
     // Tu código aquí
+    let ahora = 2025;
+    let fecha_nac = 2008;
     resultado = ahora - fecha_nac;
     alert("Edad: " + resultado);
     }
@@ -15,7 +14,9 @@ function declararVariables() {
 // Instrucciones: Completa la función para asignar el valor de 'var1' a 'var2' y luego muestra el valor de 'var2'.
 function asignarValores() {
 // Tu código aquí
-    
+    let var1 = 10
+    let var2 = var1
+    alert("El valor de var2 es el mismo que var1 \n" + "var1: " + var1 + "\nvar2: " + var2);
 }
     
 // Ejercicio 3: Operaciones matemáticas
@@ -23,16 +24,23 @@ function asignarValores() {
 // Suma 'num1','num2' y 'num3', resta 'num1' de 'num3', multiplica 'num2' por 'num3', y divide 'num1' entre '120'.
 // Muestra los resultados en un mensaje.
 function realizarOperaciones() {
-
-alert( );
-
 // Tu código aquí
+    let num1 = 240
+    let num2 = 20
+    let num3 = 30
+    let operacion1 = num1 - num3
+    let operacion2 = num2 * num3
+    let operacion3 = num1 / 120
+    alert("Número 1: " + num1 + "\nNúmero 2: " + num2 + "\nNúmero 3: " + num3 + "\nOperación1: " + operacion1 + "\nOperación 2: " + operacion2 + "\nOperación 3: " + operacion3);
 }
 
 // Ejercicio 4: Manipulación de cadenas
 // Instrucciones: Completa la función para construir un mensaje utilizando las variables 'direccion' y 'n_casa'.
 // Luego, muestra la longitud del mensaje y el antepenultimo en el mensaje. (.length)
 function manipularCadenas() {
-
 // Tu código aquí
+let direccion = "Av. Santa Rosa."
+let n_casa = 7890
+let mensaje = "Dirección: " + direccion + "\nNúmero de casa: " + n_casa
+alert(mensaje + "\n" + "Longitud del mensaje: " + mensaje.length)
 }
