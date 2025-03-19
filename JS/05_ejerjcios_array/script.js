13a83f6606090692591defa20b919b5bdde0fa61
maximilianoreyes1 <maximilianoreyes@liceovvh.cl>
2025-03-19 08:15:20 -0300

    ok

--- JS/05_ejerjcios_array/script.js
+++ JS/05_ejerjcios_array/script.js
@@ -1,5 +1,5 @@
 function suma_indices(){
     let valores = [1,2,3,4,5,6,7]
     total_suma = valores[6] + valores[5] + valores[1]
-    alert(total_suma)
+    alert("Valores del array: " + valores + "\nSuma del primer, segundo y último valor: " + total_suma)
 }
