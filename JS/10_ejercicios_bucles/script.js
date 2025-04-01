2bfd7d901615a1150b54c8caac6a128dfc2bea98
maximilianoreyes1 <maximilianoreyes@liceovvh.cl>
2025-04-01 10:43:06 -0300

    ok

--- JS/10_ejercicios_bucles/script.js
+++ JS/10_ejercicios_bucles/script.js
@@ -1,3 +1,11 @@
 function multiplicar(){
-    
+    let count = 0
+    let n = prompt("Ingrese un número")
+    let array = []
+
+    for(let i = 1; i <= 10; i++){
+        count = n * i
+        array.push(count)
+    }
+    alert(count)
 }
