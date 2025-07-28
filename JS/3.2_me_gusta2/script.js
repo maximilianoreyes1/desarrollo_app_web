bab81eb4cb2260135a8e81b044bc5fb365ade79d
maximilianoreyes1 <maximilianoreyes@liceovvh.cl>
2025-07-28 10:39:19 -0400

    ok

---
+++ JS/3.2_me_gusta2/script.js
@@ -0,0 +1,20 @@
+function sumarLike1(){
+    let like1 = document.getElementById("like1");
+    let likeCount1 = parseInt(like1.innerText);
+    likeCount1++;
+    like1.innerText = likeCount1 + ' like(s)';
+}
+
+function sumarLike2(){
+    let like2 = document.getElementById("like2");
+    let likeCount2 = parseInt(like2.innerText);
+    likeCount2++;
+    like2.innerText = likeCount2 + ' like(s)';
+}
+
+function sumarLike3(){
+    let like3 = document.getElementById("like3");
+    let likeCount3 = parseInt(like3.innerText);
+    likeCount3++;
+    like3.innerText = likeCount3 + ' like(s)';
+}
