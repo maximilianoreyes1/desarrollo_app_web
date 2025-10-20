3275cb128592ea4edfc52c300f451ce8e07e7eb9
ethan nacarate <165289385+ethannacarate@users.noreply.github.com>
2025-10-20 10:53:59 -0300

    ok

---
+++ JS/3.7_onchange/static/JS/script.js
@@ -0,0 +1,37 @@
+
+function cityAlert() {
+    alert("Loading weather report...");
+}
+
+function hideCookieBanner(element) {
+    element.parentElement.remove();
+}
+
+
+// Temperatura conversion
+var allTempSpans = document.querySelectorAll(".temp-high, .temp-low");
+var originalCelsius = [];
+
+for (var i = 0; i < allTempSpans.length; i++) {
+    var temp = parseInt(allTempSpans[i].innerText);
+    originalCelsius.push(temp);
+}
+
+function convertTemps(element) {
+    
+    var unit = element.value;
+
+    for (var i = 0; i < allTempSpans.length; i++) {
+        
+        var originalC = originalCelsius[i]; 
+
+        if (unit === 'fahrenheit') {
+            
+            var fahrenheit = Math.round((originalC * 9 / 5) + 32);
+            allTempSpans[i].innerText = fahrenheit + "°";
+        } else {
+            
+            allTempSpans[i].innerText = originalC + "°";
+        }
+    }
+}
